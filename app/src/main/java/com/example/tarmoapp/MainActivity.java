cb82f68bd12900637f8dfde8d2d3d09267a49c03
landyboi <miro.nissinen@metropolia.fi>
2022-03-01 13:47:19 +0200

    Fixed a bug with navview not working properly

--- app/src/main/java/com/example/tarmoapp/MainActivity.java
+++ app/src/main/java/com/example/tarmoapp/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         actionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
