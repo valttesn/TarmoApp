294228ebab6852e2a8c696b103e12cc5346aee19
Valtteri <valtteri.niininen@metropolia.fi>
2022-03-02 13:29:03 +0200

    Dark mode

--- app/src/main/java/com/example/tarmoapp/settingsActivity.java
+++ app/src/main/java/com/example/tarmoapp/settingsActivity.java
@@ -1,14 +1,36 @@
 package com.example.tarmoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 public class settingsActivity extends AppCompatActivity {
-
+    private static boolean dark = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button btn = (Button) findViewById(R.id.darkBtn);
+
+        if(dark == true){
+            btn.setText("Tumma");
+        }else{
+            btn.setText("Vaalea");
+        }
+    }
+
+    public void onClickDark(View view){
+        if(dark == false) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES);
+            dark = true;
+        }else if(dark == true){
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+            dark = false;
+        }
     }
 }
