f07342a867acaa2a2317fe337ba2bdc948533cc7
Timi <timi.tienhaara@metropolia.fi>
2022-03-07 17:39:18 +0200

    askelmittari

--- app/src/main/java/com/example/tarmoapp/MainActivity.java
+++ app/src/main/java/com/example/tarmoapp/MainActivity.java
@@ -4,22 +4,34 @@ package com.example.tarmoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
     TextView drinkView;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
-    public int waterDrank;
+    private TextView stepsCounter;
+    private SensorManager sensorManager;
+    private Sensor mStepCounter;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,24 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         Log.d("DMG", "onCreate() called");
         loadData();
         updateData();
+
+        stepsCounter = findViewById(R.id.stepsView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        }else{
+            stepsCounter.setText("Laskurin sensori ei ole käytössä");
+            isCounterSensorPresent = false;
+        }
     }
 
     @Override
@@ -97,11 +127,15 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
     public void updateData() {
         drinkView.setText("Olet juonut " + Integer.toString(DrinkCounter.Value()) + " desiä vettä tänään.");
     }
-
+    @Override
     public void onPause() {
         super.onPause();
         Log.i("DMG", "onPause() called");
         saveData();
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.unregisterListener(this,mStepCounter);
+        }
     }
 
     public void onCalendarClick() {
@@ -117,5 +151,25 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
     public void onFitnessTipsClick() {
         Intent fitnessTipsActivity = new Intent(MainActivity.this, FitnessTipsActivity.class);
         startActivity(fitnessTipsActivity);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor == mStepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            stepsCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.registerListener(this,mStepCounter,SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 }
